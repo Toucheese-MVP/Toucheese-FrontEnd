b0f473388fd477529cf0d9b6eb3f648977277025
ruelline <ruelline91@naver.com>
2024-12-30 15:16:53 +0900

    ♻️

--- next.config.ts
+++ next.config.ts
@@ -22,9 +22,6 @@ const nextConfig: NextConfig = {
         pathname: "/**",
       },
     ],
-    deviceSizes: [320, 420, 768, 1024, 1200],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 60,
   },
 
   webpack(config, { isServer }) {
