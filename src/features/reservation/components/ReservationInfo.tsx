60d6d7a0799e66ae48c6fbee05efc2e463cd162e
ruelline <ruelline91@naver.com>
2025-01-21 15:29:21 +0900

    ♻️ StudioTitle 컴포넌트 적용

--- src/features/reservation/components/ReservationInfo.tsx
+++ src/features/reservation/components/ReservationInfo.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import { Reservation } from "@/types/Reservated.type";
+import StudioTitle from "@/components/StudioTitle";
 
 const ReservationInfo = ({ reservation }: { reservation: Reservation }) => {
   return (
@@ -7,26 +7,11 @@ const ReservationInfo = ({ reservation }: { reservation: Reservation }) => {
       <h2 className="text-lg font-bold mb-4">기존 예약 정보</h2>
       <div className="py-4 bg-white my-4 border-b border-gray-100">
         <div className="relative flex items-center justify-between gap-3">
-          <div className="w-12 h-12 rounded-full relative overflow-hidden">
-            <Image
-              src={reservation.studioImage}
-              alt={`${reservation.studioImage} `}
-              fill
-            />
-          </div>
-          <div className="mr-auto">
-            <p className="font-semibold ">{reservation.studioName}</p>
-            <p className="text-gray-500 font-medium flex gap-1 items-center">
-              <Image
-                src="/icons/event.svg"
-                alt="back"
-                width={20}
-                height={20}
-                className="object-cover"
-              />
-              {reservation.createDate}
-            </p>
-          </div>
+          <StudioTitle
+            name={reservation.studioName}
+            profileImage={`${reservation.studioImage}`}
+            createDate={reservation.createDate}
+          />
           <div
             className={`self-start px-2 py-1 mt-2 rounded-lg border border-gray-200 bg-gray-200 font-medium ${
               reservation.status === "예약확정"
