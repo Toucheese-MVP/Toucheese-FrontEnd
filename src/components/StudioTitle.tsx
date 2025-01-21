0bac4a3e84bf7ee8ae57dd81c569e36d43e6016a
ruelline <ruelline91@naver.com>
2025-01-21 15:00:08 +0900

    ♻️ 선택적으로 예약날짜 표시추가

--- src/components/StudioTitle.tsx
+++ src/components/StudioTitle.tsx
@@ -6,6 +6,7 @@ interface StudioTitleProps {
   name: string;
   profileImage: string;
   size?: "sm" | "md" | "lg";
+  createDate?: string;
 }
 
 const DEFAULT_IMAGE = "/default/sample.png";
@@ -14,6 +15,7 @@ const StudioTitle: React.FC<StudioTitleProps> = ({
   name,
   profileImage,
   size = "md",
+  createDate, // 예약 날짜 prop 추가
 }) => {
   const [imageSrc, setImageSrc] = useState(profileImage || DEFAULT_IMAGE);
 
@@ -22,6 +24,7 @@ const StudioTitle: React.FC<StudioTitleProps> = ({
     md: "h-12 w-12 text-base",
     lg: "h-16 w-16 text-lg",
   };
+
   return (
     <div className="flex items-center gap-4">
       <div
@@ -38,11 +41,28 @@ const StudioTitle: React.FC<StudioTitleProps> = ({
         <img
           src={imageSrc}
           alt="터치즈"
-          className="object-contain"
+          className="hidden"
           onError={() => setImageSrc(DEFAULT_IMAGE)}
         />
       </div>
-      <h2 className="text-gray-800 font-semibold">{name}</h2>
+
+      <div>
+        <h2 className="text-gray-800 font-semibold">{name}</h2>
+
+        {/* 예약 날짜가 있을 경우만 표시 */}
+        {createDate && (
+          <p className="text-gray-500 font-medium flex gap-1 items-center">
+            <Image
+              src="/icons/event.svg"
+              alt="calendar"
+              width={20}
+              height={20}
+              className="object-cover"
+            />
+            {createDate}
+          </p>
+        )}
+      </div>
     </div>
   );
 };
