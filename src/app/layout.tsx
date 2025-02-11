8b02a561dd4a39a38bbd8e3887753b579516c659
ruelline <ruelline91@naver.com>
2025-02-11 16:30:11 +0900

    ♻️ 모바일 input 텍스트 입력시 zoom 방지 시작

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,10 @@ export const metadata: Metadata = {
     ],
   },
   manifest: "/manifest.json",
+  other: {
+    viewport:
+      "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+  },
 };
 
 export default function RootLayout({
