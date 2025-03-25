1ec325f7a1766d45970834d6f215fc6819db1c76
ruelline <ruelline91@naver.com>
2025-03-25 13:54:38 +0900

    [refactor]: 예약 수정 페이지 CSR 구조로 명확화
    - /reservation/[reservationId] 페이지를 CSR로 설계 전환
    - 최상단 Page에서 'use client' 선언하여 GNB 상태 제어
    - SSR 구조 제거, 클라이언트 중심 상태 관리 방식으로 리팩토링 시작

--- src/app/reservation/[reservationId]/page.tsx
+++ src/app/reservation/[reservationId]/page.tsx
@@ -1,7 +1,24 @@
-import ReservationEditWrapper from "@/features/reservation/ui/ReservationWrapper";
+"use client";
 
-function ReservationEditRoutePage() {
-  return <ReservationEditWrapper />;
+import { useEffect } from "react";
+import { useGNBStore } from "@/features/common/store/useGnbStore";
+import ReservationEdit from "@/features/reservation/ui/ReservationEdit";
+import { TopBar } from "@/features/common/components/topbar";
+
+function Page() {
+  const setShowGNB = useGNBStore((state) => state.setShowGNB);
+
+  useEffect(() => {
+    setShowGNB(false);
+    return () => setShowGNB(true);
+  }, [setShowGNB]);
+
+  return (
+    <div className="-mx-4 p-4 flex-1">
+      <TopBar showShare={false} showCart={false} message="예약 수정" />
+      <ReservationEdit />
+    </div>
+  );
 }
 
-export default ReservationEditRoutePage;
+export default Page;
