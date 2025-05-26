f703ee7d4af4c67332c14ec1c35a4eaedcd3b176
ruelline <ruelline91@naver.com>
2025-05-26 12:56:48 +0900

    [refactor] home 페이지를 ssr 기반 구조로 변경
    - page는 ssr로 변경되었으나 컴포넌트 내부 zustand사용으로 ssr의 이점을 크게 못 얻고 있습니다.
    추가 리팩토링 진행 필요

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,26 @@
-"use client";
-
-import { defaultConcept } from "@/types/Concept.type";
-import { useConcept } from "@/features/studios/hooks/useConcept";
+import { getConceptList } from "@/features/studios/hooks/useConcept";
 import ConceptCard from "@/features/home/ConceptCard";
 import Header from "@/features/common/components/header";
 import SearchBar from "@/features/searchBar/searchBar";
 
-function Home() {
-  const { data: conceptList, loading } = useConcept();
+export default async function Home() {
+  const conceptList = await getConceptList();
 
   return (
     <div className="-mt-16">
       <Header />
       <SearchBar />
       <div className="grid grid-cols-2 gap-4 w-full pb-4">
-        {loading
-          ? Array.from({ length: 6 }).map((_, index) => (
-              <ConceptCard key={index} isLoading={true} />
-            ))
-          : conceptList.map((concept: defaultConcept, index: number) => (
-              <ConceptCard
-                key={concept.id}
-                id={concept.id}
-                name={concept.name}
-                index={index}
-                isLoading={false}
-              />
-            ))}
+        {conceptList.map((concept, index) => (
+          <ConceptCard
+            key={concept.id}
+            id={concept.id}
+            name={concept.name}
+            index={index}
+            isLoading={false}
+          />
+        ))}
       </div>
     </div>
   );
 }
-
-export default Home;
